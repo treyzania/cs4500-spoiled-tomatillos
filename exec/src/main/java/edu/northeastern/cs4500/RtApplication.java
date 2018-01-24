0d3a15287de0595e7b0b3f3d1b2bc0e9af030101
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-01-24 15:21:52 -0500

    Added basic approximate layout of the Spring Boot for the homework.

---
+++ exec/src/main/java/edu/northeastern/cs4500/RtApplication.java
@@ -0,0 +1,14 @@
+package edu.northeaster.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtApplication.class, args);
+	}
+
+}
+
