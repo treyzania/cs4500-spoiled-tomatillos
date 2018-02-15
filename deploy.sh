c18b90a375c4b694583fc14e58b65eb76d55bbc7
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-14 21:32:39 -0500

    Fixed wrong container name in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,20 @@ repopath=$(dirname $(realpath $0))
 mountpath='/opt/rottentomatillos'
 
 function start_container() {
+	cd $repopath
+	jarpath=$(ls $1/target/*.jar)
 	docker run \
-		-n $2 \
+		--name=$2 \
 		-v $repopath:$mountpath \
 		$3 \
-		java -jar "$mountpath/$1/target/*.jar"
+		openjdk \
+		java -jar "$mountpath/$jarpath"
 }
+
+# Prune container if applicable.
+if [ "$1" != "--no-prune" ]; then
+	docker container prune -f
+fi
 
 # This is the REST API wrapping the database.
 start_container 'exec' 'rt-exec'
