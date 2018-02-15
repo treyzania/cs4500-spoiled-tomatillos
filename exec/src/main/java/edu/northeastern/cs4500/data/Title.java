9dc89a6795162928ae21b80513dd514ade3b0135
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-15 10:29:58 -0500

    Fixed "titles" database name on `@Entity` annotation.

--- exec/src/main/java/edu/northeastern/cs4500/data/Title.java
+++ exec/src/main/java/edu/northeastern/cs4500/data/Title.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "title")
+@Entity(name = "titles")
 public class Title {
 	
 	@Id
