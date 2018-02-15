7b1fff1820b290c3303a0db6a3bbcf4416ca0967
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-15 10:46:30 -0500

    Added entity and repository for rating represenation.

---
+++ exec/src/main/java/edu/northeastern/cs4500/data/TitleRating.java
@@ -0,0 +1,50 @@
+package edu.northeastern.cs4500.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "ratings")
+public class TitleRating {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	private Title title;
+	
+	@ManyToOne
+	private User user;
+	
+	private int rating = 0;
+	
+	public TitleRating() {
+		
+	}
+	
+	public TitleRating(Title title, User user, int rating) {
+		this.title = title;
+		this.user = user;
+		this.rating = rating;
+	}
+	
+	public Title getTitle() {
+		return this.title;
+	}
+	
+	public User getUser() {
+		return this.user;
+	}
+	
+	public int getRating() {
+		return this.rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+}
