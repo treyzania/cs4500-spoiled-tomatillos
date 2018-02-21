642ecf23429a3a0e90d4bd20338bf8108a1ed50d
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-20 20:07:29 -0500

    Added `/api/search` REST endpoint, only supporting title search for now.

--- exec/src/main/java/edu/northeastern/cs4500/data/TitleRepository.java
+++ exec/src/main/java/edu/northeastern/cs4500/data/TitleRepository.java
@@ -1,7 +1,11 @@
 package edu.northeastern.cs4500.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TitleRepository extends JpaRepository<Title, Integer> {
 
+	List<Title> findByNameLikeIgnoreCase(String inName);
+	
 }
