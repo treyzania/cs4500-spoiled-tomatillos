aa928fa28dd88e287b52801eddd4ff15a25d4d8d
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-22 17:42:32 -0500

    Split up user data endpoints into their own REST controller.

--- restapi/src/main/java/edu/northeastern/cs4500/controllers/UserDataController.java
+++ restapi/src/main/java/edu/northeastern/cs4500/controllers/UserDataController.java
@@ -1,10 +1,5 @@
 package edu.northeastern.cs4500.controllers;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +11,11 @@ import edu.northeastern.cs4500.data.AuthKey;
 import edu.northeastern.cs4500.data.AuthKeyRepository;
 import edu.northeastern.cs4500.data.Session;
 import edu.northeastern.cs4500.data.SessionRepository;
-import edu.northeastern.cs4500.data.Title;
-import edu.northeastern.cs4500.data.TitleRatingRepository;
-import edu.northeastern.cs4500.data.TitleRepository;
 import edu.northeastern.cs4500.data.User;
 import edu.northeastern.cs4500.data.UserRepository;
 
-@SuppressWarnings("unused")
 @RestController
-public class RtRestController {
+public class UserDataController {
 
 	@Autowired
 	private UserRepository userRepo;
@@ -35,15 +26,9 @@ public class RtRestController {
 	@Autowired
 	private SessionRepository sessionRepo;
 
-	@Autowired
-	private TitleRepository titleRepo;
-
-	@Autowired
-	private TitleRatingRepository ratingRepo;
-	
 	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.GET)
 	public User getUser(@PathVariable int id) {
-		return this.userRepo.getOne(Integer.valueOf(id));
+		return this.userRepo.findOne(Integer.valueOf(id));
 	}
 
 	@RequestMapping(value = "/api/user/create", method = RequestMethod.POST, params = "username")
@@ -51,25 +36,6 @@ public class RtRestController {
 		User u = new User(username);
 		this.userRepo.saveAndFlush(u);
 		return u;
-	}
-
-	@RequestMapping(value = "/api/title/{id}", method = RequestMethod.GET)
-	public Title getTitle(@PathVariable int id) {
-		return this.titleRepo.getOne(Integer.valueOf(id));
-	}
-
-	@RequestMapping(value = "/api/title/create", method = RequestMethod.POST, params = {"name", "year", "desc"})
-	public Title createTitle(
-			@RequestParam("name") String name,
-			@RequestParam("year") int year,
-			@RequestParam("desc") String description) {
-
-		Title t = new Title(name, year);
-		t.setSummary(description);
-
-		this.titleRepo.saveAndFlush(t);
-		return t;
-		
 	}
 
 	@RequestMapping(value = "/api/session/login", method = RequestMethod.POST, params = {"username", "password"})
@@ -88,22 +54,6 @@ public class RtRestController {
 			return null;
 		}
 
-	}
-
-	@RequestMapping(value = "/api/search", method = RequestMethod.GET, params = {"query"})
-	public List<Object> search(@RequestParam("query") String query) {
-		
-		// Use a set here to avoid duplicates.
-		Set<Title> titles = new HashSet<>();
-		for (String comp : query.split("\\+")) {
-			titles.addAll(this.titleRepo.findByNameLikeIgnoreCase("%" + comp + "%"));
-		}
-		
-		// Now convert it to a list, but in a flexible way.
-		List<Object> out = new ArrayList<>();
-		out.addAll(titles);
-		return out;
-		
 	}
 
 }
