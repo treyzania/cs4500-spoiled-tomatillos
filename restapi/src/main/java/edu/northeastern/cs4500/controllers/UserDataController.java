452e2b6d2ba37260328b11bfee4a640a9da6a262
James "Trey" Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-22 21:39:18 -0500

    Made user creation require a password.
    Also tweaked endpoint for getting current user.

--- restapi/src/main/java/edu/northeastern/cs4500/controllers/UserDataController.java
+++ restapi/src/main/java/edu/northeastern/cs4500/controllers/UserDataController.java
@@ -34,16 +34,24 @@ public class UserDataController {
 		return this.userRepo.findOne(Integer.valueOf(id));
 	}
 
-	@RequestMapping(value = "/api/user/create", method = RequestMethod.POST, params = "username")
-	public User createUser(@RequestParam("username") String username) {
+	@RequestMapping(value = "/api/user/create", method = RequestMethod.POST, params = {"username", "password"})
+	public User createUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+
+		// Create and commit the user data.
 		User u = new User(username);
 		this.userRepo.saveAndFlush(u);
+
+		// Then create and commit the password, doing it after the user was created in the DB so that the numbers work out.
+		AuthKey k = new AuthKey(u, password);
+		this.authRepo.saveAndFlush(k);
+
 		return u;
+
 	}
 
 	@RequestMapping(value = "/api/session/login", method = RequestMethod.POST, params = {"username", "password"})
 	public Session login(@RequestParam("username") String username, @RequestParam("password") String password) {
-		
+
 		// First we look up the user and their last auth key stuff.
 		User u = this.userRepo.findUserByUsername(username);
 		AuthKey k = this.authRepo.findFirstByUserUsername(username);
@@ -52,7 +60,7 @@ public class UserDataController {
 		if (u == null || k == null) {
 			return null;
 		}
-		
+
 		// Then a simple check to see if the password matches.
 		if (k.isMatched(password)) {
 			Session s = new Session(u);
@@ -66,7 +74,7 @@ public class UserDataController {
 
 	@RequestMapping(value = "/api/session/logout", method = RequestMethod.POST)
 	public Session logout(@CookieValue(SESSION_COOKIE_NAME) String token) {
-		
+
 		// Just query the data and logout.
 		Session s = this.sessionRepo.findByToken(token);
 		if (s != null) {
@@ -76,10 +84,12 @@ public class UserDataController {
 		} else {
 			return null;
 		}
+
 	}
 
-	public User getCurrentUser(@CookieValue(SESSION_COOKIE_NAME) String token) {
-		
+	@RequestMapping(value = "/api/user/current", method = RequestMethod.GET)
+	public User getSessionUser(@CookieValue(SESSION_COOKIE_NAME) String token) {
+
 		// Find the session by their token and return the user data referenced by the session.
 		Session s = this.sessionRepo.findByToken(token);
 		if (s != null) {
@@ -87,7 +97,7 @@ public class UserDataController {
 		} else {
 			return null;
 		}
-		
+
 	}
-	
+
 }
