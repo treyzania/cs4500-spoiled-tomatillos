e4a8b6fe351fc546f90e225a83a7a7f9a86fbb23
delbonis.j <delbonis.j@husky.neu.edu>
2018-03-29 10:31:44 -0400

    Added admin secret checking to MovieDataController.

--- restapi/src/main/java/edu/northeastern/cs4500/controllers/MovieDataController.java
+++ restapi/src/main/java/edu/northeastern/cs4500/controllers/MovieDataController.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.northeastern.cs4500.data.Title;
 import edu.northeastern.cs4500.data.TitleRatingRepository;
 import edu.northeastern.cs4500.data.TitleRepository;
+import edu.northeastern.cs4500.services.AdminSecretService;
 
 @SuppressWarnings("unused")
 @RestController
@@ -27,6 +30,9 @@ public class MovieDataController {
 
 	@Autowired
 	private TitleRatingRepository ratingRepo;
+	
+	@Autowired
+	private AdminSecretService secretService;
 
 	@RequestMapping(value = "/api/title/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Title> getTitle(@PathVariable int id) {
@@ -42,12 +48,15 @@ public class MovieDataController {
 
 	@RequestMapping(value = "/api/title/create", method = RequestMethod.POST, params = {"name", "year", "desc"})
 	public ResponseEntity<Title> createTitle(
+			@RequestHeader("Admin-Secret") String secret, 
 			@RequestParam("name") String name,
 			@RequestParam("year") int year,
 			@RequestParam(value = "desc", required = false) String description,
 			@RequestParam(value = "src", required = false) String source) {
 
-		// TODO Make this check for authority.
+		if (!this.secretService.getSuperuserSecret().equals(secret)) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
 
 		Title t = new Title(name, year, source != null ? source : "manual");
 		if (description != null) {
