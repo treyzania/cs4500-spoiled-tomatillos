dca992b782710a0d4bfa0220f18dc739c85b6dc5
delbonis.j <delbonis.j@husky.neu.edu>
2018-03-29 09:43:09 -0400

    Upgraded javax.persistence to newer version and fixed NotificationRepository codegen.

--- data/src/main/java/edu/northeastern/cs4500/data/NotificationRepository.java
+++ data/src/main/java/edu/northeastern/cs4500/data/NotificationRepository.java
@@ -1,11 +1,12 @@
 package edu.northeastern.cs4500.data;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 	
-	List<Notification> findByTargetAndPublishedAfter(User u);
+	List<Notification> findByTargetAndPublishedAfter(User u, Timestamp ts);
 	
 }
