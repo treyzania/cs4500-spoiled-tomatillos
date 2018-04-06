6f7c1cf3b330e140c07bd60baaa0801a6c4109b8
delbonis.j <delbonis.j@husky.neu.edu>
2018-04-06 17:25:30 -0400

    Set up user page, w/ basic session support.

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,7 @@ import os
 
 import urllib3
 import json
+import requests
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
@@ -13,8 +14,6 @@ import rtapi
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-http = urllib3.PoolManager()
-
 @app.route('/')
 def main_page():
 	return render_template('index.html')
@@ -23,11 +22,23 @@ def main_page():
 def route_title(movie_id=None):
 
 	# Make the database lookup
-	movieobj = json.loads(http.request('GET', rtapi.convert_rest_url('/api/title/%s' % movie_id)).data)
+	req = requests.get(rtapi.convert_rest_url('/api/title/%s' % movie_id))
+	movieobj = json.loads(req.content)
 	if movieobj is None:
-		return 'id not found'
+		return 'title not found'
 
 	# Render the template.
-	return render_template('title.html', movie=movieobj)
+	return render_template('title.html', movie=movieobj, session=rtapi.get_session())
+
+@app.route('/user/<int:user_id>')
+def route_user(user_id=None):
+
+	# Find the user's ID
+	req = requests.get(rtapi.convert_rest_url('/api/title/%s' % user_id))
+	userobj = json.loads(req.content)
+	if userobj is None:
+		return 'user not found'
+
+	return render_template('user.html', user=userobj, session=rtapi.get_session())
 
 app.run(host='0.0.0.0')
--- frontend/rtapi.py
+++ frontend/rtapi.py
@@ -1,2 +1,23 @@
+import urllib3
+import requests
+import json
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+session_cookie_name = 'sessiontoken'
+
 def convert_rest_url(endpoint):
     return 'http://172.0.0.20:8080' + endpoint
+
+def get_session():
+    # If the user didn't pass a session token then fail immediately
+    if not session_cookie_name in request.cookies:
+        return None
+
+    # Make the request, passing the session token along.
+    cookies = {session_cookie_name: request.cookies[session_cookie_name]}
+    req = requests.get(convert_rest_url('/api/user/current'), cookies=cookies)
+    if req.status_code == 200:
+        return json.loads(req.content)
+    else:
+        return None
