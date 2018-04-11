962cffc1d44ea007c09a4d10cdc8f4599bcc427f
delbonis.j <delbonis.j@husky.neu.edu>
2018-04-10 21:30:05 -0400

    Fixed spaces instead of tabs.

--- frontend/static/core.js
+++ frontend/static/core.js
@@ -49,28 +49,28 @@ function logout() {
  */
 
 function set_cookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
 	var cookie_assignment = name + "=" + (value || "") + expires + "; path=/";
-    document.cookie = cookie_assignment;
+	document.cookie = cookie_assignment;
 	console.log("Set cookie: " + name + " = " + value);
 }
 
 function get_cookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
 }
 
 function delete_cookie(name) {
-    document.cookie = name + '= ; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
+	document.cookie = name + '= ; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
 }
