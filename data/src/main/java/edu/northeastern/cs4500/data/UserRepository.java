1a24f8d2b552b26042764bcdf0fb6a1dc3deef53
delbonis.j <delbonis.j@husky.neu.edu>
2018-04-12 10:34:22 -0400

    Added user search REST endpoint.

--- data/src/main/java/edu/northeastern/cs4500/data/UserRepository.java
+++ data/src/main/java/edu/northeastern/cs4500/data/UserRepository.java
@@ -1,9 +1,12 @@
 package edu.northeastern.cs4500.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 	User findUserByUsername(String username);
+	List<User> findUserByUsernameLike(String like);
 
 }
